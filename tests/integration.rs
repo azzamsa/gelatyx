052321d44c3b76f0447e0bc75eb82281c1123383
azzamsa <me@azzamsa.com>
2022-07-14 11:40:03 +0700

    test: avoid using unwrap

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,42 +1,49 @@
 use std::path::Path;
 
+use anyhow::Result;
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
 use std::process::Command;
 
 #[test]
-fn help() {
-    let mut cmd = Command::cargo_bin("gelatyx").unwrap();
+fn help() -> Result<()> {
+    let mut cmd = Command::cargo_bin("gelatyx")?;
     cmd.arg("-h");
     cmd.assert().success().stdout(predicate::str::contains(
         "Gelatyx 🦤 Format codebease inside the docs",
     ));
+
+    Ok(())
 }
 
 #[test]
-fn missing_lang() {
-    let mut cmd = Command::cargo_bin("gelatyx").unwrap();
+fn missing_lang() -> Result<()> {
+    let mut cmd = Command::cargo_bin("gelatyx")?;
     let path = Path::new("tests").join("doesnt").join("exist");
     cmd.arg("-f").arg(path);
     cmd.assert().failure().stderr(predicate::str::contains(
         "required arguments were not provided",
     ));
+
+    Ok(())
 }
 
 #[test]
-fn file_not_found() {
-    let mut cmd = Command::cargo_bin("gelatyx").unwrap();
+fn file_not_found() -> Result<()> {
+    let mut cmd = Command::cargo_bin("gelatyx")?;
     let path = Path::new("tests").join("doesnt").join("exist");
     cmd.arg("lua").arg("-f").arg(path);
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("No such file"));
+    Ok(())
 }
 
 #[test]
-fn format_file() {
-    let mut cmd = Command::cargo_bin("gelatyx").unwrap();
+fn format_file() -> Result<()> {
+    let mut cmd = Command::cargo_bin("gelatyx")?;
     let path = Path::new("tests").join("fixtures").join("test.md");
     cmd.arg("lua").arg("-f").arg(path);
     cmd.assert().success();
+    Ok(())
 }
