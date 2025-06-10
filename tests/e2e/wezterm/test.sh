0f72f109c9a5cc84c6dc373242b16e66ad1e673d
Azzam S.A <noreply@azzamsa.com>
2025-06-10 12:29:44 +0700

    tests: add e2e test for wezterm

---
+++ tests/e2e/wezterm/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#
+# https://github.com/wezterm/wezterm/blob/main/ci/build-docs.sh
+
+git clone --depth 1 https://github.com/wezterm/wezterm.git /tmp/wezterm
+
+tracked_markdown=$(mktemp)
+trap "rm -f ${tracked_markdown}" EXIT
+find /tmp/wezterm/docs -type f | grep -E '\.(markdown|md)$' > "$tracked_markdown"
+
+cargo run -- --language lua \
+  --file-list "$tracked_markdown" \
+  --language-config tests/e2e/wezterm/stylua.toml \
+  --check || exit 1
